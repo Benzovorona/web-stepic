289c0509a13d952ee3e4dbd1d6abef7212da5663
Benzovorona <benzovorona@gmail.com>
2016-03-04 14:38:52 +0300

    django work

--- ask/qa/urls.py
+++ ask/qa/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
+url(r'$', views.test),
 url(r'^login/$', views.test, name = 'login'),
 url(r'^signup/$', views.test, name = 'signup'),
 url(r'question/(?P<pk>[0-9]+)/$', views.test, name = 'question'),
