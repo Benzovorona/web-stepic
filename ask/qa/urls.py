6386bdb30ae1624266b1db5a4e7b188df8862f7c
Benzovorona <benzovorona@gmail.com>
2016-03-04 13:54:52 +0300

    django install

---
+++ ask/qa/urls.py
@@ -0,0 +1,11 @@
+from django.conf.urls import url
+from . import views
+
+urlpatterns = [
+url(r'^login/$', views.test, name = 'login'),
+url(r'^signup/$', views.test, name = 'signup'),
+url(r'question/(?P<pk>[0-9]+)/$', views.test, name = 'question'),
+url(r'ask/$', views.test, name = 'ask'),
+url(r'popular/$', views.test, name = 'popular'),
+url(r'new/$', views.test, name = 'new'),
+]
